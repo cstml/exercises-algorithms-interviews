3b8b1dc9172d6f9f1f9b80a9330b95a549a9368a
cstml <vladposmangiu@gmail.com>
2017-09-09 19:45:48 +0100

    update

--- source.cpp
+++ source.cpp
@@ -5,6 +5,8 @@
 #include <ctime>
 using namespace std;
 
+
+
 int main()
 {
 	srand ((unsigned)time(0));
@@ -15,9 +17,11 @@ int main()
 	int t;
 	int count;
 	int x;
+        int beat_me;
 	cout<<"How many times do you want to play: ";cin>>t;
 	for (i=1;i<=t;i++)
 	{
+                beat_me=0;
 		count=0;
 		cout<<"gimme a number : ";cin>>your_number[i];
 		do{
@@ -27,9 +31,18 @@ int main()
 			random_integer=random_integer % x;
 			cout<<your_number[i]<<"   "<<random_integer<<endl;
 		}
-		while(random_integer < your_number[i]);
+		while(random_integer < your_number[i] && count<1000);
 		my_number[i]=random_integer;
-		cout<<"it took me :"<<count<<" times to beat you"<<endl;
+                if(count<1000)
+                {
+                    my_number[i]=random_integer;
+		    cout<<"it took me :"<<count<<" times to beat you"<<endl;
+                }
+                else
+                {
+                    cout<<"you beat me this time";
+                    my_number[i]=0;
+                }
 	}
 	return 0;
 }
