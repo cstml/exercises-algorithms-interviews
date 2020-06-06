2e2e17a52a6a87a266c88067224b1a4d7d663fa2
cstml <29694576+cstml@users.noreply.github.com>
2020-06-06 01:05:04 +0100

    new: created a library with a header and a class for testing

---
+++ etc/2-library/bin/draw.h
@@ -0,0 +1,12 @@
+//draw.h
+//this library draws stuff
+#ifndef DRAW_H
+#define DRAW_H
+
+class WindowSquare
+{
+    public:
+        WindowSquare();
+};
+
+#endif
---
+++ etc/2-library/bin/helloworld.h
@@ -0,0 +1,10 @@
+//Hello world library
+//much fun
+//
+
+#include <iostream>
+
+void HW()
+{
+    std::cout<<"Hello World \n";
+}
