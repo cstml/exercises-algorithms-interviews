d82d17cf0453361bb2716592b1ce0fc8696fdb91
Vlad <cstmlcodes@gmail.com>
2021-03-08 20:32:47 +0000

    Create 21_merge_sorted_lists.cpp

---
+++ leetcode/Archive/21_merge_sorted_lists.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {    
+        ListNode *p;
+        if (l1 == nullptr)
+            return l2;
+        if (l2 == nullptr)
+            return l1;
+        if (l1->val <= l2 -> val){
+            p = l1;
+            l1->next = mergeTwoLists(l1->next, l2);
+        } else {
+            p = l2;
+            l2->next = mergeTwoLists(l1, l2->next);
+        }
+        return p;
+    }
+};
